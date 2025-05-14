63110c1fe3d74e730f8e760a8c14179346a48bf7
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-05-14 16:26:56 +0700

    Update run_grpo_video.sh

--- scripts/run_grpo_video.sh
+++ scripts/run_grpo_video.sh
@@ -23,7 +23,7 @@ torchrun --nproc_per_node="2" \
     --dataset_name tv360_video \
     # --max_prompt_length 512 \
     # --max_completion_length 256 \
-    --num_generations 1 \
+    # --num_generations 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \

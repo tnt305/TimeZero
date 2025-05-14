bd95c55738cecb14c2a87630944f36dc9b26c366
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-05-14 17:50:19 +0700

    Update grpo_video.py

--- src/open_r1/grpo_video.py
+++ src/open_r1/grpo_video.py
@@ -290,6 +290,7 @@ if __name__ == "__main__":
             "--num_generations", "1",
             "--torch_dtype", "float16",
             "--attn_implementation", "eager",
+            "--load_in_4bit", "True",
             "--per_device_train_batch_size", "1",
             "--per_device_eval_batch_size", "1",
             "--lr_scheduler_type", "cosine",

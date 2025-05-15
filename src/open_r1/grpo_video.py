12ab044b66b1547210485a56188956a42dffebde
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-05-15 10:05:02 +0700

    Update grpo_video.py

--- src/open_r1/grpo_video.py
+++ src/open_r1/grpo_video.py
@@ -171,7 +171,11 @@ def get_duration(video_path):
 def load_json_dataset(train_data_path, eval_data_path, preprocessed_data_path= "./dataset"): # Modified to accept preprocessed_data_path
     max_pixels = 3584 * 28 * 28
     min_pixels = 16 * 28 * 28
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct-AWQ")
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct-AWQ",
+                                             image_size={
+        "shortest_edge": 224,
+        "longest_edge": 224
+    })
     def create_dataset_from_json(file_path, split_name):
         with open(file_path, 'r') as f:
             data = [json.loads(line) for line in f if line.strip()]

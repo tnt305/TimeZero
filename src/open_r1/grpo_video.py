357f31c7c0458d0bde386af10850e80c34c18ff3
Thien Dang <110881337+tnt305@users.noreply.github.com>
2025-05-15 09:39:51 +0700

    Update grpo_video.py

--- src/open_r1/grpo_video.py
+++ src/open_r1/grpo_video.py
@@ -171,7 +171,7 @@ def get_duration(video_path):
 def load_json_dataset(train_data_path, eval_data_path, preprocessed_data_path= "./dataset"): # Modified to accept preprocessed_data_path
     max_pixels = 3584 * 28 * 28
     min_pixels = 16 * 28 * 28
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct-GPTQ-Int4")
     def create_dataset_from_json(file_path, split_name):
         with open(file_path, 'r') as f:
             data = [json.loads(line) for line in f if line.strip()]
@@ -279,7 +279,7 @@ if __name__ == "__main__":
     default_args = [
             "--dataset_name", "tv360_video",
             "--deepspeed" ,"./scripts/zero3_offload.json",
-            "--model_name_or_path", "Qwen/Qwen2-VL-2B-Instruct",
+            "--model_name_or_path", "Qwen/Qwen2-VL-2B-Instruct-GPTQ-Int4",
             "--trust_remote_code", "True",
             "--fp16", "True",
             "--num_generations", "1",

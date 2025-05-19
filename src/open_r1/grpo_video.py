dabf88da90635cf117d4e8aedf253bbb1646105b
thiendc-isods <cao-thien.dang@isods.org>
2025-05-19 14:39:24 +0700

    Test

--- src/open_r1/grpo_video.py
+++ src/open_r1/grpo_video.py
@@ -175,7 +175,7 @@ def load_json_dataset(train_data_path, eval_data_path, preprocessed_data_path= "
     max_pixels = 3584 * 28 * 28
     min_pixels = 16 * 28 * 28
     processor = AutoProcessor.from_pretrained(
-        "/kaggle/working/TimeZero/Qwen2-VL-2B-Instruct-AWQ",
+        "/kaggle/working/Qwen2-VL-2B-Instruct",
         size = {
             "shortest_edge": 224,
             "longest_edge": 398,
@@ -305,7 +305,7 @@ if __name__ == "__main__":
     default_args = [
             "--dataset_name", "tv360_video",
             # "--deepspeed" ,"./scripts/zero3_offload.json",
-            "--model_name_or_path", "/kaggle/working/TimeZero/Qwen2-VL-2B-Instruct-AWQ",
+            "--model_name_or_path", "/kaggle/working/Qwen2-VL-2B-Instruct",
             "--bf16", "True",
             "--num_generations", "1",
             "--torch_dtype", "bfloat16",
